b264a1d9bf337c1328c9dfaa4bf57f879ef11226
kojoxc <aderhmfzn@gmail.com>
2026-07-09 14:27:10 +0700

    add: contract test transfer & transferFrom

--- test/MinimalERC20.t.sol
+++ test/MinimalERC20.t.sol
@@ -8,7 +8,8 @@ contract MinimalERC20Test is Test {
     MinimalERC20 internal token;
 
     address internal alice = address(0xA11CE);
-    address internal attacker = address(0xB0B);
+    address internal bob = address(0xB0B);
+    address internal nad = address(0xC1C);
 
     function setUp() public {
         token = new MinimalERC20("Minimal Token", "MIN");
@@ -32,11 +33,38 @@ contract MinimalERC20Test is Test {
     }
 
     function testUnauthorizedMintingReverts() public {
-        vm.prank(attacker);
+        vm.prank(bob);
         vm.expectRevert(MinimalERC20.Unauthorized.selector);
-        token.mint(attacker, 100 ether);
+        token.mint(bob, 100 ether);
 
-        assertEq(token.balanceOf(attacker), 0);
+        assertEq(token.balanceOf(bob), 0);
         assertEq(token.totalSupply(), 0);
+    }
+
+    function test_transferBetweenUsers() public {
+        token.mint(alice, 100 ether);
+
+        vm.prank(alice);
+        // token.transfer(bob, 50 ether);
+        assertTrue(token.transfer(bob, 50 ether));
+
+        assertEq(token.balanceOf(alice), 50 ether);
+        assertEq(token.balanceOf(bob), 50 ether);
+    }
+
+    function test_transferFromBetweenUsers() public {
+        token.mint(alice, 100 ether);
+
+        vm.prank(alice);
+        // token.approve(bob, 80 ether);
+        assertTrue(token.approve(bob, 80 ether));
+
+        vm.prank(bob);
+        // token.transferFrom(alice, nad, 80 ether);
+        assertTrue(token.transferFrom(alice, nad, 80 ether));
+
+        assertEq(token.balanceOf(alice), 20 ether);
+        assertEq(token.balanceOf(nad), 80 ether);
+        assertEq(token.allowance(alice, bob), 0);
     }
 }
